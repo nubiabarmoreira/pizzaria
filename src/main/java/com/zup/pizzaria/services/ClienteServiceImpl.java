5c9508c7e8eca5c0013ab8e999c91767738e82e6
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2025-01-22 15:15:47 -0300

    feat: create class client service

--- src/main/java/com/zup/pizzaria/services/ClienteServiceImpl.java
+++ src/main/java/com/zup/pizzaria/services/ClienteServiceImpl.java
@@ -1,4 +1,20 @@
 package com.zup.pizzaria.services;
 
-public class ClienteServiceImpl {
+import com.zup.pizzaria.dtos.ClienteDTO;
+import com.zup.pizzaria.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService{
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Override
+    public ClienteDTO criarCliente(String nome, String email) {
+        ClienteDTO clienteDTO = new ClienteDTO(nome, email);
+        ClienteDTO clienteSalvo = clienteRepository.save(clienteDTO);
+
+        return new ClienteDTO(clienteSalvo.getId(), clienteSalvo.getNome(), clienteSalvo.getEmail());
+    }
 }
